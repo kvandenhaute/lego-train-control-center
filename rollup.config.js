31634f2aa37ec4df4fd5b32a407571a6fc7b9830
Kevin Van Den Haute <kevin.van.den.haute@dpgmedia.be>
2020-12-11 12:02:27 +0100

    remove legacy shizzle

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 // import url from '@rollup/plugin-url';
 import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
+// import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
@@ -17,7 +17,6 @@ import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
 	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
@@ -35,10 +34,10 @@ export default {
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
-				// compilerOptions: {
+				compilerOptions: {
 					dev,
-					hydratable: true,
-				// },
+					hydratable: true
+				},
 				preprocess: sveltePreprocess({
 					postcss: {
 						plugins: [autoprefixer]
@@ -46,10 +45,6 @@ export default {
 				}),
 				emitCss: true
 			}),
-			// url({
-			// 	sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-			// 	publicPath: '/client/'
-			// }),
 			resolve({
 				browser: true,
 				dedupe: ['svelte']
@@ -58,23 +53,6 @@ export default {
 			typescript({
 				strict: false,
 				sourceMap: dev
-			}),
-
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
 			}),
 
 			!dev && terser({
@@ -95,18 +73,13 @@ export default {
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
-				// compilerOptions: {
+				compilerOptions: {
 					dev,
 					generate: 'ssr',
-					hydratable: true,
-				// },
+					hydratable: true
+				},
 				preprocess: sveltePreprocess()
 			}),
-			// url({
-			// 	sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-			// 	publicPath: '/client/',
-			// 	emitFiles: false // already emitted by client build
-			// }),
 			resolve({
 				dedupe: ['svelte']
 			}),
